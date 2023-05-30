66e10b3a1cb2cc7588958fbe2cd683deb05be7dc
Martim Santos <aluno24235@ipt.pt>
2023-05-30 09:12:13 +0100

    inicial

--- Projeto/Data/ApplicationDbContext.cs
+++ Projeto/Data/ApplicationDbContext.cs
@@ -1,10 +1,17 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Projeto.Models;
 
 namespace Projeto.Data {
     public class ApplicationDbContext : IdentityDbContext {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options) {
         }
+
+
+        public DbSet<Jogador> Jogador { get; set; }
+        public DbSet<Itens> Itens { get; set; }
+        public DbSet<Grupo> Grupo { get; set; }
+        public DbSet<Mensagem> Mensagem { get; set; }
     }
 }
